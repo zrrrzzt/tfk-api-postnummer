db371cbacab237cc07150e2650cd7fcf522f2db9
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-06-17 22:02:06 +0200

    Adds uppercase to params

--- handlers/index.js
+++ handlers/index.js
@@ -15,7 +15,7 @@ function getPostnummer(request, reply) {
 }
 
 function getPostnummerByPostnummer(request, reply) {
-  var postnummer = parseInt(request.params.postnummer, 10)
+  var postnummer = parseInt(request.params.postnummer, 10);
   zipcodes.find({'Postnummer':postnummer}, function(error, data) {
     helpers.handleReply(error, data, request, reply);
   });
@@ -28,13 +28,15 @@ function searchPostnummer(request, reply) {
 }
 
 function getPostnummerByKommunenavn(request, reply) {
-  zipcodes.find({'Kommunenavn':request.params.kommunenavn}, function(error, data) {
+  var kommunenavn = request.params.kommunenavn.toUpperCase();
+  zipcodes.find({'Kommunenavn':kommunenavn}, function(error, data) {
     helpers.handleReply(error, data, request, reply);
   });
 }
 
 function getPostnummerByPoststed(request, reply) {
-  zipcodes.find({'Poststed':request.params.poststed}, function(error, data) {
+  var poststed = request.params.poststed.toUpperCase();
+  zipcodes.find({'Poststed': poststed}, function(error, data) {
     helpers.handleReply(error, data, request, reply);
   });
 }
@@ -42,7 +44,8 @@ function getPostnummerByPoststed(request, reply) {
 function getPostnummerByKategori(request, reply) {
   var skipNum = request.query.skip ? parseInt(request.query.skip, 10):0;
   var limitNum = request.query.limit ? parseInt(request.query.limit, 10):20;
-  zipcodes.find({'Kategori':request.params.kategori}).skip(skipNum).limit(limitNum, function(error, data) {
+  var kategori = request.params.kategori.toUpperCase();
+  zipcodes.find({'Kategori':kategori}).skip(skipNum).limit(limitNum, function(error, data) {
     helpers.handleReply(error, data, request, reply);
   });
 }

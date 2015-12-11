c5c0f27aa0ac4e63dad5459a5836269934e8262b
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-12-11 12:59:25 +0100

    Adds support for process.env variables. Matches standard codestyle

--- config/index.js
+++ config/index.js
@@ -1,8 +1,8 @@
-'use strict';
+'use strict'
 
 var config = {
-  'SERVER_PORT':3000,
-  'DB': 'mongodb://localhost:27017/tfk'
-};
+  'SERVER_PORT': process.env.SERVER_PORT || 3000,
+  'DB': process.env.DB || 'mongodb://192.168.99.100:27017/tfk'
+}
 
-module.exports = config;
+module.exports = config
